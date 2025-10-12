d266ae1554a6ebb1e05af8faa94d42e3c3d8da21
CraftsManShip001 <seoj080829@gmail.com>
2025-10-12 22:04:52 +0900

    feat :: docsBlcok으로 모듈화

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,21 +1,24 @@
 import { DocsLayout } from "@/components/layout/DocsLayout";
 import { DocsHeader } from "@/components/docs/DocsHeader";
-import { DocsSection } from "@/components/docs/DocsSection";
-import { DocsParagraph } from "@/components/docs/DocsParagraph";
+import { DocsBlock } from "@/components/docs/DocsBlock";
 
 export default function DocsPage() {
   return (
     <DocsLayout>
       <DocsHeader title="시작하기" breadcrumb={["가이드"]}/>
-      <DocsSection title="시작하기">
-        <DocsParagraph>
-          테스트 환경 주의점, 브라우저 설정 및 플랫폼을 알아보세요.
-        </DocsParagraph>
-
-        <DocsParagraph>
-          BSSM Developers는 개발자 편의를 위해 라이브 환경과 유사한 테스트 환경을 제공합니다.
-        </DocsParagraph>
-      </DocsSection>
+      <DocsBlock module="headline_1">시작하기</DocsBlock>
+      <DocsBlock module="space"/>
+      <DocsBlock module="docs_1">
+        테스트 환경 주의점, 방화벽 설정, 지원 플랫폼 및 브라우저를 알아보세요.
+      </DocsBlock>
+      <DocsBlock module="big_space"/>
+      <DocsBlock module="headline_1">테스트 환경</DocsBlock>
+      <DocsBlock module="space"/>
+      <DocsBlock module="space"/>
+      <DocsBlock module="space"/>
+      <DocsBlock module="docs_1">
+        BSSM Developers는 개발자의 편의를 위해 라이브 환경과 유사한 테스트 환경을 제공합니다.
+      </DocsBlock>
     </DocsLayout>
   );
 }
